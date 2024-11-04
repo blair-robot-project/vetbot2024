d3be4b622303fce0773867aa8bb8bf9ff5b1be12
Rakshay N. <rakshay2108@gmail.com>
2024-11-03 23:40:29 -0500

    motionmagic elevator and simulation

--- src/main/kotlin/frc/team449/robot2024/constants/subsystem/ElevatorConstants.kt
+++ src/main/kotlin/frc/team449/robot2024/constants/subsystem/ElevatorConstants.kt
@@ -2,19 +2,26 @@ package frc.team449.robot2024.constants.subsystem
 
 import com.ctre.phoenix6.signals.InvertedValue
 import com.ctre.phoenix6.signals.NeutralModeValue
+import edu.wpi.first.wpilibj.util.Color8Bit
 import kotlin.math.PI
 
 object ElevatorConstants {
 
-  const val UPDATE_FREQUENCY = 50.0
+  const val ANGLE = 30.0 // TODO: someone figure this out
+  const val MIN_LENGTH = 0.1
+  const val WIDTH = 10.0
+  val COLOR = Color8Bit(255, 0, 255)
+  val DESIRED_COLOR = Color8Bit(0, 255, 0)
+
+  const val UPDATE_FREQUENCY = 50.0 // hz
   const val DUTY_CYCLE_DEADBAND = 0.001
   val NEUTRAL_MODE = NeutralModeValue.Brake
   val ORIENTATION = InvertedValue.Clockwise_Positive
 
   const val BURST_TIME_LIMIT = 0.25
   const val BURST_CURRENT_LIMIT = 60.0
-  const val SUPPLY_CURRENT_LIMIT = 40.0
-  const val STATOR_CURRENT_LIMIT = 150.0
+  const val SUPPLY_CURRENT_LIMIT = 30.0
+  const val STATOR_CURRENT_LIMIT = 80.0
   const val MOTOR_ID = 0
 
   const val GEARING_MOTOR_TO_GEAR = 25.0 / 1.0
@@ -22,10 +29,17 @@ object ElevatorConstants {
   const val MOTOR_ROTS_TO_ELEVATOR_IN = GEARING_MOTOR_TO_GEAR * 1 / (GEAR_DIAMETER_IN * PI)
   const val GEARING = MOTOR_ROTS_TO_ELEVATOR_IN
 
-  const val RIGHT_KS = 1.0
-  const val RIGHT_KV = 1.0
-  const val RIGHT_KA = 1.0
-  const val RIGHT_KP = 1.0
-  const val RIGHT_KI = 1.0
-  const val RIGHT_KD = 1.0
+  const val STOW_HEIGHT = 1.0 // m
+  const val HIGH_HEIGHT = 2.0 // m
+
+  const val MM_ACCEL = 4.9 // m/s^2
+  const val MM_VEL = 20.0 // m/s
+
+  const val KS = 0.0
+  const val KV = 0.0
+  const val KA = 0.0
+  const val KG = 0.0
+  const val KP = 0.0
+  const val KI = 0.0
+  const val KD = 0.0
 }
--- src/main/kotlin/frc/team449/robot2024/subsystems/Elevator.kt
+++ src/main/kotlin/frc/team449/robot2024/subsystems/Elevator.kt
@@ -1,10 +1,15 @@
 package frc.team449.robot2024.subsystems
 
-import com.ctre.phoenix6.SignalLogger
+import com.ctre.phoenix6.BaseStatusSignal
 import com.ctre.phoenix6.configs.TalonFXConfiguration
-import com.ctre.phoenix6.controls.PositionVoltage
+import com.ctre.phoenix6.controls.MotionMagicVoltage
 import com.ctre.phoenix6.hardware.TalonFX
+import com.ctre.phoenix6.signals.GravityTypeValue
 import edu.wpi.first.util.sendable.SendableBuilder
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.team449.robot2024.constants.subsystem.ElevatorConstants
@@ -13,38 +18,62 @@ class Elevator(
   private val motor: TalonFX
 ) : SubsystemBase() {
 
-  private val positionRequest = PositionVoltage(0.0)
+  // simulation
+  private val mech = Mechanism2d(1.5, 2.0)
+  private val rootElevator: MechanismRoot2d = mech.getRoot("elevator", 0.25, 0.25)
+  private val elevatorVisual: MechanismLigament2d = rootElevator.append(
+    MechanismLigament2d(
+      "elevator",
+      ElevatorConstants.MIN_LENGTH,
+      ElevatorConstants.ANGLE,
+      ElevatorConstants.WIDTH,
+      ElevatorConstants.COLOR
+    )
+  )
+
+  private val rootDesiredElevator: MechanismRoot2d = mech.getRoot("desiredElevator", 0.20, 0.2735)
+  private val desiredElevatorVisual: MechanismLigament2d = rootDesiredElevator.append(
+    MechanismLigament2d(
+      "desiredElevator",
+      ElevatorConstants.MIN_LENGTH,
+      ElevatorConstants.ANGLE,
+      ElevatorConstants.WIDTH,
+      ElevatorConstants.DESIRED_COLOR
+    )
+  )
+
+  private val request = MotionMagicVoltage(ElevatorConstants.STOW_HEIGHT)
     .withSlot(0)
     .withEnableFOC(false)
-    .withUpdateFreqHz(50.0)
-
-  init {
-    name = "Elevator"
-    motor.optimizeBusUtilization()
-
-    SignalLogger.setPath("/media/sda1/ctre-logs/")
-    SignalLogger.start()
-  }
-
-  fun moveToPosition(position: Double) {
-    motor.setControl(positionRequest.withPosition(position))
-  }
+    .withUpdateFreqHz(1000.0)
 
   fun setPosition(position: Double): Command {
-    return this.runOnce { moveToPosition(position) }
+    return this.runOnce { motor.setControl(request.withPosition(position)) }
+  }
+
+  fun stop(): Command {
+    return this.runOnce { motor.stopMotor() }
+  }
+
+  override fun periodic() {
+    elevatorVisual.length = ElevatorConstants.MIN_LENGTH + motor.position.value
+    desiredElevatorVisual.length = ElevatorConstants.MIN_LENGTH + request.Position
+
+    SmartDashboard.putData("Elevator Visual", mech)
   }
 
   override fun initSendable(builder: SendableBuilder) {
-    builder.publishConstString("1.0", "Motor Stuff")
-    builder.addDoubleProperty("1.1 Front Voltage", { motor.motorVoltage.value }, null)
-    builder.addDoubleProperty("1.2 Front Position", { motor.position.value }, null)
-    builder.addDoubleProperty("1.3 Front Velocity", { motor.velocity.value }, null)
-    builder.addDoubleProperty("1.4 Desired Position", { positionRequest.Position }, null)
+    builder.publishConstString("1.0", "Elevator Info")
+    builder.addDoubleProperty("1.1 Voltage", { motor.motorVoltage.value }, null)
+    builder.addDoubleProperty("1.2 Position", { motor.position.value }, null)
+    builder.addDoubleProperty("1.3 Velocity", { motor.velocity.value }, null)
+    builder.addDoubleProperty("1.4 Desired Position", { request.Position }, null)
   }
 
   companion object {
     fun createElevator(): Elevator {
       val motor = TalonFX(ElevatorConstants.MOTOR_ID)
+
       val config = TalonFXConfiguration()
       config.CurrentLimits.StatorCurrentLimitEnable = true
       config.CurrentLimits.SupplyCurrentLimitEnable = true
@@ -52,20 +81,36 @@ class Elevator(
       config.CurrentLimits.SupplyCurrentLimit = ElevatorConstants.SUPPLY_CURRENT_LIMIT
       config.CurrentLimits.SupplyCurrentThreshold = ElevatorConstants.BURST_CURRENT_LIMIT
       config.CurrentLimits.SupplyTimeThreshold = ElevatorConstants.BURST_TIME_LIMIT
-      config.Slot0.kS = ElevatorConstants.RIGHT_KS
-      config.Slot0.kV = ElevatorConstants.RIGHT_KV
-      config.Slot0.kA = ElevatorConstants.RIGHT_KA
-      config.Slot0.kP = ElevatorConstants.RIGHT_KP
-      config.Slot0.kI = ElevatorConstants.RIGHT_KI
-      config.Slot0.kD = ElevatorConstants.RIGHT_KD
+
+      config.Slot0.kS = ElevatorConstants.KS
+      config.Slot0.kV = ElevatorConstants.KV
+      config.Slot0.kA = ElevatorConstants.KA
+      config.Slot0.kP = ElevatorConstants.KP
+      config.Slot0.kI = ElevatorConstants.KI
+      config.Slot0.kD = ElevatorConstants.KD
+      config.Slot0.kG = ElevatorConstants.KG
+      config.Slot0.GravityType = GravityTypeValue.Elevator_Static
+
+      config.MotionMagic.MotionMagicAcceleration = ElevatorConstants.MM_ACCEL
+      config.MotionMagic.MotionMagicCruiseVelocity = ElevatorConstants.MM_VEL
+
       config.MotorOutput.Inverted = ElevatorConstants.ORIENTATION
       config.MotorOutput.NeutralMode = ElevatorConstants.NEUTRAL_MODE
       config.MotorOutput.DutyCycleNeutralDeadband = ElevatorConstants.DUTY_CYCLE_DEADBAND
       config.Feedback.SensorToMechanismRatio = ElevatorConstants.GEARING
+
       motor.configurator.apply(config)
-      motor.velocity.setUpdateFrequency(ElevatorConstants.UPDATE_FREQUENCY)
-      motor.motorVoltage.setUpdateFrequency(ElevatorConstants.UPDATE_FREQUENCY)
-      motor.closedLoopError.setUpdateFrequency(ElevatorConstants.UPDATE_FREQUENCY)
+
+      BaseStatusSignal.setUpdateFrequencyForAll(
+        ElevatorConstants.UPDATE_FREQUENCY,
+        motor.position,
+        motor.velocity,
+        motor.motorVoltage,
+        motor.supplyCurrent,
+        motor.torqueCurrent,
+        motor.deviceTemp
+      )
+
       motor.optimizeBusUtilization()
 
       return Elevator(motor)
