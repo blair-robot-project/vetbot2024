dd6b1d172955bc3a73615f1a29ca9625a1a4ef42
Adam Kofner <adamdkofner@gmail.com>
2024-11-22 15:28:38 -0500

    Wait Command not needed

--- src/main/kotlin/frc/team449/auto/routines/Stack3.kt
+++ src/main/kotlin/frc/team449/auto/routines/Stack3.kt
@@ -22,7 +22,7 @@ class Stack3(
     ChoreoRoutine(
       drive = robot.drive,
       parallelEventMap = hashMapOf(
-        0 to WaitCommand(1.28).andThen(pickup(robot)),
+        0 to pickup(robot),
         1 to stowAndHold(robot)
       ),
       stopEventMap = hashMapOf(
@@ -42,4 +42,3 @@ class Stack3(
       ChoreoTrajectory.createTrajectory(arrayListOf("S3-B1", "B1-S2"), "Stack3")
     }
 }
-
